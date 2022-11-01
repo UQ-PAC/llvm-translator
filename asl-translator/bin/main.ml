727c5e6b2f04d0e8b547131a53fca84087380de5
Kait Lam <k@rina.fyi>
2022-11-01 11:57:22 +1000

    handle basic block linking.

--- asl-translator/bin/main.ml
+++ asl-translator/bin/main.ml
@@ -3,4 +3,8 @@
 let () = 
   let open Test.Translate in
   let stmts = load_aslb Sys.argv.(1) in 
-  ignore @@ translate_stmts stmts
+  print_newline ();
+  List.iter (fun x -> x |> LibASL.Asl_utils.pp_stmt |> print_endline) stmts;
+  print_newline ();
+  ignore @@ translate_stmts stmts;
+  Llvm.dump_module llmodule
--- asl-translator/lib/translate.ml
+++ asl-translator/lib/translate.ml
@@ -6,20 +6,13 @@ let a = 2
 
 let ctx : llcontext = Llvm.global_context ()
 
-let entry : llmodule = Llvm.create_module ctx "entryx"
+let llmodule : llmodule = Llvm_irreader.parse_ir ctx (Llvm.MemoryBuffer.of_file "./lib/state.ll")
 
 let void_t = void_type ctx
-let t : lltype = function_type void_t [| |]
+let void_fun_t : lltype = function_type void_t [| |]
 
-let func : llvalue = define_function "main" void_t entry
-let entry = entry_block func
-
-let _ = 
-  let build = builder_at_end ctx entry in 
-  build_ret_void build 
-   
-
-let f () = dump_value func
+let func : llvalue = declare_function "main" void_fun_t llmodule
+(* let entry = entry_block func *)
 
 let translate_prim (nm: string) (tes: expr list) (es: expr list) : llvalue = 
   match nm,tes,es with 
@@ -40,14 +33,13 @@ let ident = function | Ident x | FIdent (x,_) -> x
 
 let rec translate_expr (exp : expr) : llvalue = 
   match exp with 
-  | Expr_Field (_, _) -> assert false
-  | Expr_Slices (_, _) -> undef void_t
+  | Expr_Field (_, _) -> undef (i1_type ctx)
+  | Expr_Slices (_, _) -> undef (i1_type ctx)
   | Expr_In (_, _) -> assert false
   | Expr_Var _ -> assert false
-  | Expr_Array (_, _) -> undef void_t
+  | Expr_Array (_, _) -> undef (i1_type ctx)
   | Expr_Parens e -> translate_expr e
   | Expr_TApply (nm, tes, es) -> translate_prim (ident nm) tes es
-  | Expr_If (_, _, _, _, _) -> assert false
   
   | Expr_LitInt _ -> assert false
   | Expr_LitHex _ -> assert false
@@ -57,6 +49,7 @@ let rec translate_expr (exp : expr) : llvalue =
   | Expr_LitMask _ 
   | Expr_LitString _ 
   
+  | Expr_If (_, _, _, _, _)
   | Expr_Fields (_, _)  
   | Expr_Tuple _        
   | Expr_Binop (_, _, _)
@@ -65,21 +58,31 @@ let rec translate_expr (exp : expr) : llvalue =
   | Expr_Unknown _      
   -> failwith @@ "unhandled translate_expr: " ^ pp_expr exp
 
+(** a tuple of entry and exit basic blocks. 
 
+    has the following properties: 
+    - the two blocks are linked, with the entry block 
+      suitable for entering the concept described
+    - the exit block does not have a terminating instruction
+    - the two blocks may refer to the same block if there is only one.
+ *)
+type blockpair = llbasicblock * llbasicblock
 
-let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock = 
+let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock * llbasicblock = 
   let block = append_block ctx "" func in
   let build = builder_at_end ctx block in
-  let () = match stmt with 
-  | Stmt_Assert _ -> ()
-  | Stmt_VarDeclsNoInit (_ty, _nms, _) -> ()
+  let single = (block, block) in
+
+  match stmt with 
+  | Stmt_Assert _ -> single
+  | Stmt_VarDeclsNoInit (_ty, _nms, _) -> single
   | Stmt_VarDecl (ty, nm, rv, _)  
   | Stmt_ConstDecl (ty, nm, rv, _) ->
     let ty = translate_ty ty in
     let alloc = build_alloca ty (ident nm) build in
-    ignore @@ build_store (translate_expr rv) alloc build
-    
-  | Stmt_Assign (_, _, _) -> ()
+    let _ = build_store (translate_expr rv) alloc build in
+    single
+  | Stmt_Assign (_, _, _) -> single
   | Stmt_Throw (_, _) -> assert false
   | Stmt_TCall (_, _, _, _) -> assert false
   | Stmt_If (_, _, _, _, _) -> assert false
@@ -102,18 +105,18 @@ let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock =
   | Stmt_FunReturn (_, _) 
   | Stmt_ProcReturn _ 
   -> failwith @@ "unhandled translate_stmt: " ^ pp_stmt stmt
-  in 
-  block
 
-let translate_stmts (stmts : stmt list) : llbasicblock = 
-  let bbs = List.map translate_stmt stmts in
-  let insts bb = fold_right_instrs (fun inst l -> inst::l) bb [] in
-  let flattened = List.concat (List.map insts bbs) in
-
-  let block = append_block ctx "block" func in
-  let build = builder_at_end ctx block in
-  List.iter (fun inst -> insert_into_builder inst (value_name inst) build) flattened;
-  block
+let rec translate_stmts (stmts : stmt list) : llbasicblock * llbasicblock = 
+  match stmts with 
+  | [] -> 
+    let b = append_block ctx "" func in b,b
+  | s::rest -> 
+    let (b1, b2) = translate_stmt s in
+    let (rest1, rest2) = translate_stmts rest in
+    
+    let builder = builder_at_end ctx b2 in
+    ignore @@ build_br rest1 builder;
+    (b1, rest2)
 
   
 let load_aslb f = 
