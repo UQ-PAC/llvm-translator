16bbc77764abc5971a12b3859c60b4f7d3bb81a7
Kait Lam <k@rina.fyi>
2022-11-01 16:03:25 +1000

    work towards ASL translation.
    variable mapping, lexpr, slices, pc update.

--- asl-translator/bin/main.ml
+++ asl-translator/bin/main.ml
@@ -2,9 +2,14 @@
 
 let () = 
   let open Test.Translate in
-  let stmts = load_aslb Sys.argv.(1) in 
+  let fname = Sys.argv.(1) in
+  let stmts = load_aslb fname in 
   print_newline ();
   List.iter (fun x -> x |> LibASL.Asl_utils.pp_stmt |> print_endline) stmts;
   print_newline ();
-  ignore @@ translate_stmts stmts;
-  Llvm.dump_module llmodule
+  ignore @@ translate_stmts_entry stmts;
+  
+  Llvm.dump_module llmodule;
+  match Llvm_analysis.verify_module llmodule with 
+  | None -> () 
+  | Some s -> failwith @@ "verify_module failed: " ^ s
--- asl-translator/lib/translate.ml
+++ asl-translator/lib/translate.ml
@@ -11,7 +11,7 @@ let llmodule : llmodule = Llvm_irreader.parse_ir ctx (Llvm.MemoryBuffer.of_file 
 let void_t = void_type ctx
 let void_fun_t : lltype = function_type void_t [| |]
 
-let func : llvalue = declare_function "main" void_fun_t llmodule
+let func : llvalue = define_function "main" void_fun_t llmodule
 (* let entry = entry_block func *)
 
 let translate_prim (nm: string) (tes: expr list) (es: expr list) : llvalue = 
@@ -31,24 +31,57 @@ let translate_ty (ty: ty) : lltype =
 
 let ident = function | Ident x | FIdent (x,_) -> x
 
-let rec translate_expr (exp : expr) : llvalue = 
+module Globals = Map.Make(String)
+let vars : llvalue Globals.t ref = ref Globals.empty
+
+
+let unknown_loc = LibASL.Asl_ast.Unknown
+let lexpr_to_expr = LibASL.Symbolic.lexpr_to_expr unknown_loc
+let expr_to_lexpr = LibASL.Symbolic.expr_to_lexpr
+
+
+let rec translate_expr (exp : expr) (build: llbuilder) : llvalue = 
+  let err s = failwith @@ "translate_expr: " ^ s ^ " at " ^ pp_expr exp in
   match exp with 
-  | Expr_Field (_, _) -> undef (i1_type ctx)
-  | Expr_Slices (_, _) -> undef (i1_type ctx)
-  | Expr_In (_, _) -> assert false
-  | Expr_Var _ -> assert false
-  | Expr_Array (_, _) -> undef (i1_type ctx)
-  | Expr_Parens e -> translate_expr e
+  | Expr_Slices (base, [Slice_LoWd (Expr_LitInt lo,Expr_LitInt wd)]) ->    
+    let base = translate_expr base build in
+    let lo = int_of_string lo and wd = int_of_string wd in
+    let ty = type_of base in
+    let ty' = integer_type ctx wd in
+    
+    let lshr = 
+      if lo = 0 
+        then base 
+        else build_lshr base (const_int ty lo) "" build
+    in
+    if integer_bitwidth ty' = integer_bitwidth ty
+      then lshr 
+      else build_trunc lshr ty' "" build
+  
+  | Expr_Var (Ident "TRUE") -> const_all_ones (i1_type ctx)
+  | Expr_Var (Ident "FALSE") -> const_null (i1_type ctx)
+  
+  | Expr_Var _ 
+  | Expr_Array _ 
+  | Expr_Field (_, _) -> 
+    let ptr = translate_lexpr (expr_to_lexpr exp) in
+    build_load ptr "" build 
+
+  | Expr_Parens e -> translate_expr e build
   | Expr_TApply (nm, tes, es) -> translate_prim (ident nm) tes es
   
   | Expr_LitInt _ -> assert false
   | Expr_LitHex _ -> assert false
-  | Expr_LitBits _ -> assert false
-  
+  | Expr_LitBits bits -> 
+    let ty = integer_type ctx (String.length bits) in
+    const_int ty (int_of_string ("0b" ^ bits))
+    
   | Expr_LitReal _ 
   | Expr_LitMask _ 
   | Expr_LitString _ 
-  
+      
+  | Expr_Slices _
+  | Expr_In (_, _) 
   | Expr_If (_, _, _, _, _)
   | Expr_Fields (_, _)  
   | Expr_Tuple _        
@@ -56,7 +89,34 @@ let rec translate_expr (exp : expr) : llvalue =
   | Expr_Unop (_, _)    
   | Expr_ImpDef (_, _)  
   | Expr_Unknown _      
-  -> failwith @@ "unhandled translate_expr: " ^ pp_expr exp
+  -> err "unsupported expression"
+
+
+and translate_lexpr (lexp: lexpr) : llvalue = 
+  let err s = failwith @@ "translate_lexpr: " ^ s ^ " at " ^ pp_lexpr lexp in
+  let find nm = 
+    try Globals.find nm !vars 
+    with Not_found -> err @@ "cannot find variable " ^ nm
+  in
+
+  match lexp with 
+  | LExpr_Var (Ident "_PC") -> find "PC"
+  | LExpr_Var nm -> find (ident nm)
+  
+  | LExpr_Array (LExpr_Var (Ident "_R"), Expr_LitInt n) -> find ("X"^n)
+  | LExpr_Field (LExpr_Var (Ident "PSTATE"), Ident "nRW") -> find "nRW"
+
+  | LExpr_Array _
+  | LExpr_Wildcard
+  | LExpr_Field (_, _)
+  | LExpr_Fields (_, _)
+  | LExpr_Slices (_, _)
+  | LExpr_BitTuple _
+  | LExpr_Tuple _
+  | LExpr_Write (_, _, _)
+  | LExpr_ReadWrite (_, _, _, _) -> err "unsupported"
+
+
 
 (** a tuple of entry and exit basic blocks. 
 
@@ -68,6 +128,7 @@ let rec translate_expr (exp : expr) : llvalue =
  *)
 type blockpair = llbasicblock * llbasicblock
 
+
 let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock * llbasicblock = 
   let block = append_block ctx "" func in
   let build = builder_at_end ctx block in
@@ -75,14 +136,26 @@ let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock * llbasicblock =
 
   match stmt with 
   | Stmt_Assert _ -> single
-  | Stmt_VarDeclsNoInit (_ty, _nms, _) -> single
+  | Stmt_VarDeclsNoInit (ty, nms, _) -> 
+    let ty = translate_ty ty in
+    List.iter (fun nm -> 
+      let alloc = build_alloca ty (ident nm) build in
+      vars := Globals.add (ident nm) alloc !vars;
+    ) nms;
+    single
+
   | Stmt_VarDecl (ty, nm, rv, _)  
   | Stmt_ConstDecl (ty, nm, rv, _) ->
     let ty = translate_ty ty in
     let alloc = build_alloca ty (ident nm) build in
-    let _ = build_store (translate_expr rv) alloc build in
+    ignore @@ build_store (translate_expr rv build) alloc build;
+
+    vars := Globals.add (ident nm) alloc !vars;
     single
-  | Stmt_Assign (_, _, _) -> single
+  | Stmt_Assign (le, rv, _) -> 
+    let rv = translate_expr rv build in
+    ignore @@ build_store rv (translate_lexpr le) build;
+    single
   | Stmt_Throw (_, _) -> assert false
   | Stmt_TCall (_, _, _, _) -> assert false
   | Stmt_If (_, _, _, _, _) -> assert false
@@ -106,17 +179,67 @@ let translate_stmt (stmt : LibASL.Asl_ast.stmt) : llbasicblock * llbasicblock =
   | Stmt_ProcReturn _ 
   -> failwith @@ "unhandled translate_stmt: " ^ pp_stmt stmt
 
+let link (first: blockpair) (second: blockpair): blockpair = 
+    let (b1, b2) = first in
+    let (rest1, rest2) = second in
+    ignore @@ build_br rest1 (builder_at_end ctx b2);
+    (b1, rest2)
+
+
 let rec translate_stmts (stmts : stmt list) : llbasicblock * llbasicblock = 
   match stmts with 
   | [] -> 
     let b = append_block ctx "" func in b,b
   | s::rest -> 
-    let (b1, b2) = translate_stmt s in
-    let (rest1, rest2) = translate_stmts rest in
-    
-    let builder = builder_at_end ctx b2 in
-    ignore @@ build_br rest1 builder;
-    (b1, rest2)
+    let s' = translate_stmt s in 
+    let rest' = translate_stmts rest in 
+    link s' rest'
+
+
+let branchtaken_fix tl : blockpair =
+  let entry_builder = builder_at_end ctx tl in
+
+  let exit = append_block ctx "" func in
+
+  let inc_block = append_block ctx "" func in
+  let inc_build = builder_at_end ctx inc_block in
+
+  let pc_ptr = Globals.find "PC" !vars in
+  let pc = build_load pc_ptr "" inc_build in
+  let inc = build_add pc (const_int (type_of pc) 4) "" inc_build in
+  ignore @@ build_store inc pc_ptr inc_build;
+  ignore @@ build_br exit inc_build;
+
+  let branchtaken_ptr = Globals.find "__BranchTaken" !vars in
+  let bt = build_load branchtaken_ptr "" entry_builder in
+  ignore @@ build_cond_br bt exit inc_block entry_builder;
+
+  tl, exit
+
+
+
+let translate_stmts_entry (stmts: stmt list) = 
+  vars := fold_left_globals (fun bs g -> Globals.add (value_name g) g bs) Globals.empty llmodule;
+
+  let entry = entry_block func in
+  let entry_builder = builder_at_end ctx entry in
+
+  let branchtaken = build_alloca (i1_type ctx) "__BranchTaken" entry_builder in
+  let locals = List.to_seq [
+    branchtaken;
+    build_alloca (integer_type ctx 2) "BTypeNext" entry_builder;
+    build_alloca (i1_type ctx) "nRW" entry_builder;
+  ]
+  in  
+  vars := Globals.add_seq (Seq.map (fun x -> (value_name x, x)) locals) !vars;
+
+  let stmts' = translate_stmts stmts in
+  let (hd,tl) = link stmts' (branchtaken_fix (snd stmts')) in
+  ignore @@ build_br hd entry_builder;
+  ignore @@ build_ret_void (builder_at_end ctx tl);
+
+  hd,tl
+  
 
   
 let load_aslb f = 
